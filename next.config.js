5ceaf4cd5124fcd87fa17df83e1406dab02a7fd0
nerkartran297 <nerkar.tran@gmail.com>
2025-01-04 14:57:56 +0700

    Updated for browser linting

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   experimental: {
     mdxRs: true,
     serverComponentsExternalPackages: ["mongoose"],
+    serverActions: true,  // thêm này để tối ưu server actions
   },
   images: {
     remotePatterns: [
@@ -18,6 +19,25 @@ const nextConfig = {
         hostname: "*",
       },
     ],
+  },
+  // Thêm các tối ưu cho production
+  poweredByHeader: false,
+  compress: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  // Thêm config cho webpack để tối ưu bundle size
+  webpack: (config, { dev, isServer }) => {
+    // Chỉ tối ưu ở production
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+    return config;
   },
 };
 
