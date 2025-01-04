4814ede21c12ee8d62c3f24bc8057ce9be29a996
nerkartran297 <nerkar.tran@gmail.com>
2025-01-04 14:28:14 +0700

    Updated for browser

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -15,74 +15,74 @@ import type { SearchParamsProps } from "@/types";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Collection — DevOverflow",
+    title: "Collection — DevOverflow",
 };
 
 export default async function Collection({ searchParams }: SearchParamsProps) {
-  const { userId: clerkId } = auth();
+    const { userId: clerkId } = auth();
 
-  if (!clerkId) return null;
+    if (!clerkId) return null;
 
-  const mongoUser = await getUserById({ userId: clerkId });
-  if (!mongoUser?.onboarded) redirect("/onboarding");
+    const mongoUser = await getUserById({ userId: clerkId });
+    if (!mongoUser?.onboarded) redirect("/onboarding");
 
-  const result = await getSavedQuestions({
-    clerkId,
-    searchQuery: searchParams.q,
-    filter: searchParams.filter,
-    page: searchParams.page ? +searchParams.page : 1,
-  });
+    const result = await getSavedQuestions({
+        clerkId,
+        searchQuery: searchParams.q,
+        filter: searchParams.filter,
+        page: searchParams.page ? +searchParams.page : 1,
+    });
 
-  return (
-    <>
-      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
-      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-        <LocalSearchbar
-          route="/collection"
-          iconPosition="left"
-          imgSrc="/assets/icons/search.svg"
-          placeholder="Search for questions"
-          otherClasses="flex-1"
-        />
+    return (
+        <>
+            <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+            <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+                <LocalSearchbar
+                    route="/collection"
+                    iconPosition="left"
+                    imgSrc="/assets/icons/search.svg"
+                    placeholder="Search for questions"
+                    otherClasses="flex-1"
+                />
 
-        <Filter
-          filters={QuestionFilters}
-          otherClasses="min-h-[56px] sm:min-w-[170px]"
-        />
-      </div>
+                <Filter
+                    filters={QuestionFilters}
+                    otherClasses="min-h-[56px] sm:min-w-[170px]"
+                />
+            </div>
 
-      <div className="mt-10 flex w-full flex-col gap-6">
-        {result.questions.length > 0 ? (
-          result.questions.map((question: any) => (
-            <QuestionCard
-              key={question._id}
-              _id={question._id}
-              clerkId={clerkId}
-              title={question.title}
-              tags={question.tags}
-              author={question.author}
-              upvotes={question.upvotes}
-              views={question.views}
-              answers={question.answers}
-              createdAt={question.createdAt}
-            />
-          ))
-        ) : (
-          <NoResult
-            title="No Saved Questions Found"
-            description="It appears that there are no saved questions in your collection at the moment 😔. Start exploring and saving questions that pique your interest 🌟"
-            link="/"
-            linkTitle="Explore Questions"
-          />
-        )}
-      </div>
+            <div className="mt-10 flex w-full flex-col gap-6">
+                {result.questions.length > 0 ? (
+                    result.questions.map((question: any) => (
+                        <QuestionCard
+                            key={question._id}
+                            _id={question._id}
+                            clerkId={clerkId}
+                            title={question.title}
+                            tags={question.tags}
+                            author={question.author}
+                            upvotes={question.upvotes}
+                            views={question.views}
+                            answers={question.answers}
+                            createdAt={question.createdAt}
+                        />
+                    ))
+                ) : (
+                    <NoResult
+                        title="No Saved Questions Found"
+                        description="It appears that there are no saved questions in your collection at the moment 😔. Start exploring and saving questions that pique your interest 🌟"
+                        link="/"
+                        linkTitle="Explore Questions"
+                    />
+                )}
+            </div>
 
-      <div className="mt-10">
-        <Pagination
-          pageNumber={searchParams?.page ? +searchParams.page : 1}
-          isNext={result.isNext}
-        />
-      </div>
-    </>
-  );
+            <div className="mt-10">
+                <Pagination
+                    pageNumber={searchParams?.page ? +searchParams.page : 1}
+                    isNext={result.isNext}
+                />
+            </div>
+        </>
+    );
 }
